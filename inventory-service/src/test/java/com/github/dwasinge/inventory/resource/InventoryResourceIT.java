a99d6cc47177e5d1ea225f4596af8c07316929ac
Derek Wasinger <dwasinge@dwasinge.remote.csb>
2020-03-14 09:43:56 -0700

    fixes for deserializer and initial data load

--- inventory-service/src/test/java/com/github/dwasinge/inventory/resource/InventoryResourceIT.java
+++ inventory-service/src/test/java/com/github/dwasinge/inventory/resource/InventoryResourceIT.java
@@ -1,40 +1,26 @@
 package com.github.dwasinge.inventory.resource;
 
-import static io.restassured.RestAssured.given;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-
-import org.junit.jupiter.api.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dwasinge.inventory.domain.InventoryDetail;
 
-
 public class InventoryResourceIT {
 
-	@Test
-	public void testLoadInventory() throws JsonProcessingException {
+	// @Test
+	public void createLoadInventoryJson() throws IOException {
 
 		List<InventoryDetail> initialDetails = mockInventory();
 
 		ObjectMapper mapper = new ObjectMapper();
 		String json = mapper.writeValueAsString(initialDetails);
-		
-		System.out.println(json);
 
-//        given()
-//        .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
-//        .header("Content-Type", MediaType.APPLICATION_JSON)
-//    .when()
-//        .post("/fruits")
-//    .then()
-//        .statusCode(200)
-		
-		
+		Files.write(Paths.get("/tmp/inventory-load.json"), json.getBytes());
+
 	}
 
 	private List<InventoryDetail> mockInventory() {
@@ -42,21 +28,18 @@ public class InventoryResourceIT {
 		List<InventoryDetail> detailList = new ArrayList<>();
 
 		// for each store
-		for(int i = 0; i < 9; i++) {
+		for (int i = 0; i < 9; i++) {
 
 			int storeId = 1000 + i;
 
-			System.out.println("current store: " + storeId);
+			for (int j = 1; j < 145; j++) {
 
-			for(int j = 1; j < 145; j++) {
-
-				System.out.println("\tcurrent item: " + j);
 				detailList.add(new InventoryDetail(storeId, j, 25));
 
 			}
-			
+
 		}
-		
+
 		return detailList;
 
 	}
